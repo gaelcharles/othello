8b2c8f7bdc143f27064a726c033db96c9eadf797
GaelCharles <gael.charles@gmail.com>
2017-04-15 21:32:21 +0200

    amelioration rendu graphique + virage conflits

---
+++ Console.cpp
@@ -0,0 +1,62 @@
+#include "Console.h"
+#include <conio.h>
+#include <windows.h>
+
+Console* Console::m_instance = NULL;
+
+Console::Console()
+{
+    m_instance = NULL;
+}
+
+Console::~Console()
+{
+
+}
+
+void Console::deleteInstance()
+{
+    delete m_instance;
+    m_instance = NULL;
+}
+
+Console* Console::getInstance()
+{
+    if (!Console::m_instance)
+    {
+        m_instance = new Console();
+    }
+
+    return m_instance;
+}
+
+
+void Console::gotoLigCol(int lig, int col)
+{
+    COORD mycoord;
+    mycoord.X = col;
+    mycoord.Y = lig;
+    SetConsoleCursorPosition( GetStdHandle( STD_OUTPUT_HANDLE ), mycoord );
+}
+
+void Console::gotoDamierXY(int x, int y)
+{
+    gotoLigCol(2*y+1, 4*x+2);
+}
+
+int Console::getInputKey()
+{
+    return getch();
+}
+
+bool Console::isKeyboardPressed()
+{
+    return kbhit();
+}
+
+void Console::setColor(e_color texte, e_color fond)
+{
+    HANDLE H=GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(H,fond*16+texte);
+}
+
---
+++ Constantes.h
@@ -0,0 +1,26 @@
+#ifndef CONSTANTES_H_INCLUDED
+#define CONSTANTES_H_INCLUDED
+
+/// MACROS ///
+
+#define BORDURE_ANGLE_HD char(191)
+#define BORDURE_ANGLE_BD char(217)
+#define BORDURE_ANGLE_HG char(218)
+#define BORDURE_ANGLE_BG char(192)
+#define BORDURE_BARRE_HORZ char(196)
+#define BORDURE_BARRE_VERT char(179)
+#define BORDURE_INTER3_HAUT char(193)
+#define BORDURE_INTER3_BAS char(194)
+#define BORDURE_INTER3_DROITE char(195)
+#define BORDURE_INTER3_GAUCHE char(180)
+#define BORDURE_INTER4 char(197)
+
+#define CARAC_CARRE char(254)
+
+/// CONSTANTES ///
+
+#define TAILLE_PLATEAU 8
+#define LIGNE_AFFICHAGE 5
+#define COLONNE_AFFICHAGE 4
+
+#endif // CONSTANTES_H_INCLUDED
